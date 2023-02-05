5f585aae6c4b625e43ea64bcf48135de5d4b16b1
Urchinode <vrdhan212@gmail.com>
2023-02-05 23:00:50 +0900

    feat: setup flutter linter
    Using flutter_lints package

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
