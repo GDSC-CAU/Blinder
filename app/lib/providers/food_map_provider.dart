8787222ca9b1b535b2334ca67678bb5a3f903ded
Danpa_cho <danpa725@cau.ac.kr>
2023-02-12 00:00:40 +0900

    refactor: update food map provider with `ModelFactory` to handle method actions

--- app/lib/providers/food_map_provider.dart
+++ app/lib/providers/food_map_provider.dart
@@ -1,22 +1,24 @@
 import 'package:app/models/food_map.dart';
 import 'package:app/models/food_menu.dart';
+import 'package:app/models/model_factory.dart';
 import 'package:flutter/material.dart';
 
 class FoodMapProvider extends ChangeNotifier {
-  final List<FoodMap> foodMap = [];
+  final foodMap = ModelFactory(FoodMap());
 
   FoodMapProvider();
 
   List<String> getFoodCategory() =>
-      foodMap.map((food) => food.category).toList();
+      foodMap.dataList.map((food) => food.category).toList();
 
   List<FoodMenu> getFoodMenuByCategory(String category) =>
-      foodMap.firstWhere((food) => food.category == category).menu;
+      foodMap.dataList.firstWhere((food) => food.category == category).menu;
 
-  void updateFoodMap(List<FoodMap> newFoodMap) {
-    foodMap.clear();
-    foodMap.addAll(newFoodMap);
-
+  void initializeFoodMapFromJson(List<JsonMap> jsonList) {
+    foodMap.serializeList(jsonList);
     notifyListeners();
   }
+
+  List<JsonMap> getDeserializedFoodMap() =>
+      ModelFactory.deserializeList(foodMap.dataList);
 }
