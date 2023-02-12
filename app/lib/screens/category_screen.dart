e99cc8e5907f36e342921a34f721a25f4ea117a2
Danpa_cho <danpa725@cau.ac.kr>
2023-02-12 18:04:45 +0900

    feat: add base category screen

---
+++ app/lib/screens/category_screen.dart
@@ -0,0 +1,15 @@
+import 'package:app/common/widgets/screen_layout.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenLayout(
+      onPressed: () {},
+      routeText: "",
+      children: const [],
+    );
+  }
+}
