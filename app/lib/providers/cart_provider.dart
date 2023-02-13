4e3617661645c9c910c3e49b639de9db441d0b6a
Danpa_cho <danpa725@cau.ac.kr>
2023-02-13 10:39:22 +0900

    refactor: shorten cart provider code and rename class property `items` into `foodCartItems`

--- app/lib/providers/cart_provider.dart
+++ app/lib/providers/cart_provider.dart
@@ -1,30 +1,33 @@
+import 'package:app/models/food_cart_item.dart';
 import 'package:flutter/material.dart';
 
-import '../models/item.dart';
+class FoodCartProvider with ChangeNotifier {
+  final List<FoodCartItem> foodCartItems = [];
 
-class CartProvider with ChangeNotifier {
-  final List<Item> items = [];
-
-  void addItem(Item newItem) {
-    items.add(newItem);
+  void addItem(FoodCartItem newFoodCartItem) {
+    foodCartItems.add(newFoodCartItem);
     notifyListeners();
   }
 
-  void printItems() {
-    items.map((item) => item.toString()).forEach(print);
-  }
+  List<DataRow> getFoodCartItemList() => foodCartItems
+      .map(
+        (item) => DataRow(
+          cells: [
+            DataCell(Text(item.name)),
+            DataCell(Text("${item.count}")),
+            DataCell(Text("${item.price}")),
+          ],
+        ),
+      )
+      .toList();
 
-  List<DataRow> getItemList() {
-    return items
-        .map(
-          (item) => DataRow(
-            cells: [
-              DataCell(Text(item.name)),
-              DataCell(Text(item.count.toString())),
-              DataCell(Text(item.price.toString())),
-            ],
-          ),
-        )
-        .toList();
-  }
+  @override
+  String toString() => foodCartItems
+      .map(
+        (item) => item.toString(),
+      )
+      .fold(
+        "",
+        (prev, itemString) => "$prev$itemString",
+      );
 }
