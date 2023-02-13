56be09540fd3d6c4fd355c0a83ece14f282360ea
Urchinode <vrdhan212@gmail.com>
2023-02-14 01:16:46 +0900

    fix: menu button overflow bug

--- app/lib/screens/food_category_screen.dart
+++ app/lib/screens/food_category_screen.dart
@@ -27,7 +27,7 @@ class FoodCategoryScreen extends StatelessWidget {
             ]),
           ),
           Flexible(
-            flex: 5,
+            flex: 7,
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Expanded(
