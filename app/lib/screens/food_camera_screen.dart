62981943faae78438fc09f5714974bee2a93f172
Danpa_cho <danpa725@cau.ac.kr>
2023-02-13 09:20:04 +0900

    feat: add food camera screen

---
+++ app/lib/screens/food_camera_screen.dart
@@ -0,0 +1,15 @@
+import 'package:app/common/widgets/screen_layout.dart';
+import 'package:flutter/material.dart';
+
+class FoodCameraScreen extends StatelessWidget {
+  const FoodCameraScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenLayout(
+      onPressed: () {},
+      routeText: "",
+      children: const [],
+    );
+  }
+}
