c9e46fca6c6b3128f8fef1464d688ab822c2c9d4
Urchinode <vrdhan212@gmail.com>
2023-02-14 01:58:34 +0900

    feat: complete Food Counting Screen
    Applied state management and TTS

--- app/lib/screens/food_category_screen.dart
+++ app/lib/screens/food_category_screen.dart
@@ -27,7 +27,7 @@ class FoodCategoryScreen extends StatelessWidget {
             ]),
           ),
           Flexible(
-            flex: 7,
+            flex: 9,
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Expanded(
--- app/lib/screens/food_counting_screen.dart
+++ app/lib/screens/food_counting_screen.dart
@@ -1,9 +1,12 @@
 import 'package:app/common/widgets/app_scaffold.dart';
 import 'package:app/common/widgets/bottom_button.dart';
+import 'package:app/common/widgets/screen_title.dart';
+import 'package:app/models/food_menu.dart';
 import 'package:app/utils/tts.dart';
 import 'package:flutter/material.dart';
 
 import '../common/widgets/menu_button.dart';
+import '../router/app_router.dart';
 
 class FoodCountingScreen extends StatefulWidget {
   const FoodCountingScreen({super.key});
@@ -13,8 +16,18 @@ class FoodCountingScreen extends StatefulWidget {
 }
 
 class _FoodCountingScreenState extends State<FoodCountingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      final menu = ModalRoute.of(context)!.settings.arguments! as FoodMenu;
+      ttsController.speak('${menu.name}을 선택하셨습니다. 메뉴 개수를 정해주세요.');
+    });
+  }
+
   var itemCnt = 0;
 
+  // Item Count
   void increment() => setState(() {
         itemCnt++;
         ttsController.speak('추가하기. 현재 $itemCnt개 선택했습니다.');
@@ -31,35 +44,55 @@ class _FoodCountingScreenState extends State<FoodCountingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final menuName = ModalRoute.of(context)?.settings.arguments;
+    final menu = ModalRoute.of(context)!.settings.arguments! as FoodMenu;
     return AppScaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Column(
-          children: [
-            Text(
-              menuName.toString(),
+        Flexible(
+          flex: 9,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ScreenTitle(
+                  title: menu.name,
+                ),
+                ScreenTitle(
+                  title: '$itemCnt개',
+                ),
+                const SizedBox(height: 50),
+                MenuButton(
+                  text: '추가하기',
+                  onPressed: increment,
+                ),
+                const SizedBox(height: 30),
+                MenuButton(
+                  text: '빼기',
+                  onPressed: decrement,
+                ),
+              ],
             ),
-            Text(
-              '$itemCnt개',
-            ),
-            MenuButton(
-              text: '추가하기',
-              onPressed: increment,
-            ),
-            MenuButton(
-              text: '빼기',
-              onPressed: decrement,
-            ),
-          ],
+          ),
         ),
-        Expanded(
+        Flexible(
           child: Align(
             alignment: Alignment.bottomCenter,
             child: BottomButton(
-              text: '장바구니 담기',
-              onPressed: () {},
+              text: '장바구니에 담기',
+              onPressed: () {
+                // Count Validation
+                if (itemCnt == 0) {
+                  ttsController.speak('메뉴를 1개 이상 선택해주세요.');
+                } else {
+                  AppRouter.move(
+                    context,
+                    to: RouterPath.foodOrder,
+                    arguments: '',
+                  );
+                }
+              },
             ),
           ),
         ),
