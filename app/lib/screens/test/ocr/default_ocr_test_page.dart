238632d060cd7a5e718c54e507df81ccf174a26d
Urchinode <vrdhan212@gmail.com>
2023-02-15 01:53:38 +0900

    feat: add text recognition using `google_ml_kit`

--- app/lib/screens/test/ocr/default_ocr_test_page.dart
+++ app/lib/screens/test/ocr/default_ocr_test_page.dart
@@ -1,7 +1,7 @@
 import 'package:app/common/widgets/app_scaffold.dart';
 import 'package:app/common/widgets/menu_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tesseract_ocr/android_ios.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
 
 class DefaultOcrTestPage extends StatefulWidget {
@@ -20,33 +20,41 @@ class _DefaultOcrTestPageState extends State<DefaultOcrTestPage> {
     );
 
     if (_pickedImage != null) {
-      print('선택한 이미지 경로: ${_pickedImage.path}');
-      final _extractedText = await FlutterTesseractOcr.extractText(
-        _pickedImage.path,
-        language: 'kor+eng',
-      );
+      final inputImage = InputImage.fromFilePath(_pickedImage.path);
+      final textRecognizer = GoogleMlKit.vision
+          .textRecognizer(script: TextRecognitionScript.korean);
+      final recognisedText = await textRecognizer.processImage(inputImage);
+      await textRecognizer.close();
+
+      final scannedText = StringBuffer();
+
+      for (final TextBlock block in recognisedText.blocks) {
+        for (final TextLine line in block.lines) {
+          scannedText.write('${line.text}\n');
+        }
+      }
       setState(() {
-        extractedText = _extractedText;
+        extractedText = scannedText.toString();
       });
     } else {
       print('이미지 선택 중단');
     }
   }
 
-  void extractTextFromImage() {}
-
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MenuButton(
-            text: '이미지 고르기',
-            onPressed: _extractTextFromImage,
-          ),
-          Text(extractedText),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MenuButton(
+              text: '이미지 고르기',
+              onPressed: _extractTextFromImage,
+            ),
+            Text(extractedText),
+          ],
+        ),
       ),
     );
   }
