ba9ae64d8d8007b96a1d1aadf4f59efab49092b9
Danpa_cho <danpa725@cau.ac.kr>
2023-02-15 12:39:55 +0900

    refactor: extract initialize process into setupPackages

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,12 +5,18 @@ import 'package:app/utils/tts.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
+Future<void> setupPackages() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await initTts();
   await initializeCameraInstance(
     resolution: ResolutionPreset.max,
   );
+  appCameraController.destroyController();
+}
+
+Future<void> main() async {
+  await setupPackages();
 
   runApp(const Blinder());
 }
