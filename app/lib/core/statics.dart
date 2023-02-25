b49fcd0140b80428c7aa23f81e150d601696d5be
Danpa_cho <danpa725@cau.ac.kr>
2023-02-25 15:26:01 +0900

    feat: add `getSideStepPoint` method to remove untrusted data

--- app/lib/core/statics.dart
+++ app/lib/core/statics.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:app/utils/array_util.dart';
+import 'package:app/utils/array.dart';
 
 class Statics {
   Statics();
@@ -23,6 +23,95 @@ class Statics {
                   value + pow(number - numbersAvg, 2).toInt() ~/ numbersLength,
             ),
     );
+  }
+
+  /// get side step points
+  /// ```dart
+  /// final steps = Statics.getSideStepPoint(...numbers);
+  /// final firstStep = steps["first"];
+  /// final lastStep = steps["last"];
+  /// ```
+  static Map<String, int?> getSideStepPoint(
+    List<int> numbers,
+  ) {
+    numbers.sort();
+    final numbersDiff = numbers.folder<List<int>>(
+      [],
+      (diffs, number, i, tot) {
+        if (i == tot.length - 1) {
+          return diffs;
+        }
+        final nextNumber = tot[i + 1];
+        final diff = nextNumber - number;
+        if (diff == 0) {
+          return diffs;
+        }
+        diffs.add(diff);
+        return diffs;
+      },
+    );
+    final avgOfNumberDiff = avg(numbersDiff);
+    final stdOfNumberDiff = std(numbersDiff);
+
+    final stepPointList = numbers.folder<List<int>>(
+      [],
+      (pointList, number, currentIndex, tot) {
+        if (currentIndex == tot.length - 1) return pointList;
+
+        final nextNumber = tot[currentIndex + 1];
+        final currentDiff = nextNumber - number;
+
+        final isStepIn = currentDiff <= avgOfNumberDiff + stdOfNumberDiff / 2;
+        if (isStepIn) return pointList;
+
+        pointList.add(currentIndex + 1);
+        return pointList;
+      },
+    );
+
+    if (stepPointList.isEmpty) {
+      return {
+        "first": null,
+        "last": null,
+      };
+    }
+    if (stepPointList.length == 1) {
+      final isClosedToFirst = stepPointList.first <= numbers.length / 2;
+      return isClosedToFirst
+          ? {
+              "first": stepPointList.first - 1,
+              "last": null,
+            }
+          : {
+              "first": null,
+              "last": stepPointList.first - 1,
+            };
+    }
+
+    final isBothClosedToFirst = stepPointList.first <= numbers.length / 2 &&
+        stepPointList.last <= numbers.length / 2;
+
+    if (isBothClosedToFirst) {
+      return {
+        "first": stepPointList.first - 1,
+        "last": null,
+      };
+    }
+
+    final isBothClosedToLast = stepPointList.first > numbers.length / 2 &&
+        stepPointList.last > numbers.length / 2;
+
+    if (isBothClosedToLast) {
+      return {
+        "first": null,
+        "last": stepPointList.last,
+      };
+    }
+
+    return {
+      "first": stepPointList.first - 1,
+      "last": stepPointList.last,
+    };
   }
 
   static int getQuartileAt(
