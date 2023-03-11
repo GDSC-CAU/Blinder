e8ea9c80b2f43e982b808196f93dc877aaf9db16
Danpa_cho <danpa725@cau.ac.kr>
2023-03-11 20:53:44 +0900

    refactor: remove unused deps

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import flutter_tts
 import path_provider_foundation
-import tflite_flutter_helper
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  TfliteFlutterHelperPlugin.register(with: registry.registrar(forPlugin: "TfliteFlutterHelperPlugin"))
 }
