4cbbf212e0bd518121a1dbd13627aaaf9294b13c
Danpa_cho <danpa725@cau.ac.kr>
2023-03-12 21:40:17 +0900

    fix: resolve conflict in object detector

--- app/lib/ml/object_detector.dart
+++ app/lib/ml/object_detector.dart
@@ -8,30 +8,38 @@ import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
+enum ObjectDetectorState {
+  beforeInitialized,
+  initialized,
+  destroyed,
+  error,
+}
+
 class ObjectDetectorView extends StatefulWidget {
   @override
   State<ObjectDetectorView> createState() => _ObjectDetectorView();
 }
 
 class _ObjectDetectorView extends State<ObjectDetectorView> {
-  late ObjectDetector _objectDetector;
-  bool _canProcess = false;
-  bool _isBusy = false;
+  late final ObjectDetector _objectDetector;
+
+  bool _isDetectProcessing = false;
+
+  ObjectDetectorState _objectDetectorState =
+      ObjectDetectorState.beforeInitialized;
+
   CustomPaint? _customPaint;
-  String? _text;
 
   @override
   void initState() {
     super.initState();
 
-    _initializeDetector(
-      DetectionMode.stream,
-    );
+    _initializeMenuBoardDetector();
   }
 
   @override
   void dispose() {
-    _canProcess = false;
+    _objectDetectorState = ObjectDetectorState.destroyed;
     _objectDetector.close();
 
     super.dispose();
@@ -40,76 +48,18 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
   @override
   Widget build(BuildContext context) {
     return CameraView(
-      title: "이거 되냐?",
-      text: _text,
       customPaint: _customPaint,
-      onImage: (inputImage) {
-        processImage(inputImage);
+      handleImage: (videoImage) async {
+        detectMenuBoard(videoImage);
       },
     );
   }
 
-  Future<void> _initializeDetector(
-    DetectionMode mode,
-  ) async {
-    print('Set detector in mode: $mode');
-
-    const path = 'assets/ml/menu-detector.tflite';
-    final modelPath = await _getModel(path);
-    final options = LocalObjectDetectorOptions(
-      mode: mode,
-      modelPath: modelPath,
-      classifyObjects: true,
-      multipleObjects: true,
-    );
-
-    _objectDetector = ObjectDetector(options: options);
-    _canProcess = true;
-  }
-
-  Future<void> processImage(
-    InputImage inputImage,
-  ) async {
-    if (!_canProcess) return;
-
-    if (_isBusy) return;
-
-    _isBusy = true;
-    setState(() {
-      _text = '';
-    });
-    final objects = await _objectDetector.processImage(inputImage);
-
-    if (inputImage.inputImageData?.size != null &&
-        inputImage.inputImageData?.imageRotation != null) {
-      final painter = ObjectDetectorPainter(
-        objects,
-        inputImage.inputImageData!.imageRotation,
-        inputImage.inputImageData!.size,
-      );
-      _customPaint = CustomPaint(
-        painter: painter,
-      );
-    } else {
-      String text = 'Objects found: ${objects.length}\n\n';
-      for (final object in objects) {
-        text +=
-            'Object:  trackingId: ${object.trackingId} - ${object.labels.map((e) => e.text)}\n\n';
-      }
-      _text = text;
-      // TODO: set _customPaint to draw boundingRect on top of image
-      _customPaint = null;
-    }
-    _isBusy = false;
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
-  Future<String> _getModel(String assetPath) async {
+  Future<String> _getModelFromAsset(String assetPath) async {
     if (io.Platform.isAndroid) {
       return 'flutter_assets/$assetPath';
     }
+
     final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
     await io.Directory(dirname(path)).create(
       recursive: true,
@@ -128,5 +78,57 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
     }
 
     return file.path;
+  }
+
+  Future<void> _initializeMenuBoardDetector() async {
+    const modelSourcePath = 'assets/ml/menu-detector.tflite';
+    final modelPath = await _getModelFromAsset(modelSourcePath);
+
+    final options = LocalObjectDetectorOptions(
+      mode: DetectionMode.stream,
+      modelPath: modelPath,
+      classifyObjects: true,
+      multipleObjects: true,
+    );
+
+    _objectDetector = ObjectDetector(
+      options: options,
+    );
+    _objectDetectorState = ObjectDetectorState.initialized;
+  }
+
+  /// ML detecting by image stream, [InputImage]
+  Future<void> detectMenuBoard(
+    InputImage inputImage,
+  ) async {
+    if (_objectDetectorState == ObjectDetectorState.destroyed ||
+        _objectDetectorState == ObjectDetectorState.error) return;
+
+    if (_isDetectProcessing == true) return;
+    _isDetectProcessing = true;
+
+    try {
+      final recognizedObjectList =
+          await _objectDetector.processImage(inputImage);
+
+      final isInputImageExist = inputImage.inputImageData?.size != null &&
+          inputImage.inputImageData?.imageRotation != null;
+
+      if (isInputImageExist) {
+        final painter = ObjectDetectorPainter(
+          detectedObjectList: recognizedObjectList,
+          rotation: inputImage.inputImageData!.imageRotation,
+          absoluteSize: inputImage.inputImageData!.size,
+        );
+        _customPaint = CustomPaint(
+          painter: painter,
+        );
+      }
+    } catch (mlError) {
+      print(mlError);
+      _objectDetectorState = ObjectDetectorState.error;
+    }
+
+    _isDetectProcessing = false;
   }
 }
