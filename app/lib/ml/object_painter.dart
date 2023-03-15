86aee758bebbc254437c4d7eb25eea0f698811af
Danpa_cho <danpa725@cau.ac.kr>
2023-03-15 16:17:33 +0900

    feat: add painting option in `ObjectPainter`

--- app/lib/ml/object_painter.dart
+++ app/lib/ml/object_painter.dart
@@ -1,28 +1,32 @@
 import 'dart:ui';
 import 'dart:ui' as ui;
 
+import 'package:app/ml/coordinates_translator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
-
-import 'coordinates_translator.dart';
 
 class ObjectDetectorPainter extends CustomPainter {
   final List<DetectedObject> detectedObjectList;
   final Size absoluteSize;
   final InputImageRotation rotation;
+  final Color _color;
+  final double _strokeWidth;
 
   ObjectDetectorPainter({
     required this.detectedObjectList,
     required this.rotation,
     required this.absoluteSize,
-  });
+    required Color color,
+    required double strokeWidth,
+  })  : _color = color,
+        _strokeWidth = strokeWidth;
 
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 3.0
-      ..color = Colors.red;
+      ..strokeWidth = _strokeWidth
+      ..color = _color;
 
     final Paint background = Paint()..color = const Color(0x99000000);
 
@@ -47,25 +51,25 @@ class ObjectDetectorPainter extends CustomPainter {
 
       builder.pop();
 
-      final left = translateX(
+      final left = CoordTranslator.translateX(
         detectedObject.boundingBox.left,
         rotation,
         size,
         absoluteSize,
       );
-      final top = translateY(
+      final top = CoordTranslator.translateY(
         detectedObject.boundingBox.top,
         rotation,
         size,
         absoluteSize,
       );
-      final right = translateX(
+      final right = CoordTranslator.translateX(
         detectedObject.boundingBox.right,
         rotation,
         size,
         absoluteSize,
       );
-      final bottom = translateY(
+      final bottom = CoordTranslator.translateY(
         detectedObject.boundingBox.bottom,
         rotation,
         size,
