ae8c46a96ecd1934d980f4fe652b23e3cb3505ab
Danpa_cho <danpa725@cau.ac.kr>
2023-03-15 16:16:57 +0900

    refactor: utility functions into class static members

--- app/lib/ml/coordinates_translator.dart
+++ app/lib/ml/coordinates_translator.dart
@@ -3,42 +3,54 @@ import 'dart:ui';
 
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 
-double translateX(
-  double x,
-  InputImageRotation rotation,
-  Size size,
-  Size absoluteImageSize,
-) {
-  switch (rotation) {
-    case InputImageRotation.rotation90deg:
-      return x *
-          size.width /
-          (Platform.isIOS ? absoluteImageSize.width : absoluteImageSize.height);
-    case InputImageRotation.rotation270deg:
-      return size.width -
-          x *
-              size.width /
-              (Platform.isIOS
-                  ? absoluteImageSize.width
-                  : absoluteImageSize.height);
-    default:
-      return x * size.width / absoluteImageSize.width;
-  }
-}
+class CoordTranslator {
+  CoordTranslator();
 
-double translateY(
-  double y,
-  InputImageRotation rotation,
-  Size size,
-  Size absoluteImageSize,
-) {
-  switch (rotation) {
-    case InputImageRotation.rotation90deg:
-    case InputImageRotation.rotation270deg:
-      return y *
-          size.height /
-          (Platform.isIOS ? absoluteImageSize.height : absoluteImageSize.width);
-    default:
-      return y * size.height / absoluteImageSize.height;
+  static double translateX(
+    double x,
+    InputImageRotation rotation,
+    Size size,
+    Size absoluteImageSize,
+  ) {
+    switch (rotation) {
+      case InputImageRotation.rotation90deg:
+        return x *
+            size.width /
+            (Platform.isIOS
+                ? absoluteImageSize.width
+                : absoluteImageSize.height);
+
+      case InputImageRotation.rotation270deg:
+        return size.width -
+            x *
+                size.width /
+                (Platform.isIOS
+                    ? absoluteImageSize.width
+                    : absoluteImageSize.height);
+
+      default:
+        return x * size.width / absoluteImageSize.width;
+    }
+  }
+
+  static double translateY(
+    double y,
+    InputImageRotation rotation,
+    Size size,
+    Size absoluteImageSize,
+  ) {
+    switch (rotation) {
+      case InputImageRotation.rotation90deg:
+
+      case InputImageRotation.rotation270deg:
+        return y *
+            size.height /
+            (Platform.isIOS
+                ? absoluteImageSize.height
+                : absoluteImageSize.width);
+
+      default:
+        return y * size.height / absoluteImageSize.height;
+    }
   }
 }
